03f9b84416ed8f830be41a5770cd7e4e98d73612
Miroslav Tsarev <mtzarev@gmail.com>
2024-07-18 23:15:22 +0300

    Update 8. Array Manipulator.js

--- 8. Array Manipulator.js
+++ 8. Array Manipulator.js
@@ -1,10 +1,9 @@
 function arrayManipulator(nums, commands) {
     for (let command of commands) {
-        let tokens = command.split(` `)
+        let tokens = command.split(` `);
         let action = tokens.shift();
         console.log(action);
     }
-    
 }
 arrayManipulator([1, 2, 3, 4, 5],
    ['addMany 5 9 8 7 6 5', 'contains 15', 'remove 3', 'shift 1', 'print']);
