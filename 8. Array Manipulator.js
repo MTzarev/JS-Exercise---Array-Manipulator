5b6460330c7600d3710ae76067924eb233e16cf5
Miroslav Tsarev <mtzarev@gmail.com>
2024-07-18 23:13:52 +0300

    Update 8. Array Manipulator.js

--- 8. Array Manipulator.js
+++ 8. Array Manipulator.js
@@ -4,45 +4,7 @@ function arrayManipulator(nums, commands) {
         let action = tokens.shift();
         console.log(action);
     }
-    //     if (action === "add") {
-    //         let idx = Number(tokens.shift());
-    //         let item = Number(tokens.shift());
-    //         nums.splice(idx, 0, item);
-    //     } else if (action === "addMany") {
-    //         let idx = Number(tokens.shift());
-    //         for (item of tokens) {
-    //             item = Number(item);
-    //             nums.splice(idx, 0, item);
-    //             idx++;
-    //         }
-    //     } else if (action === "contains") {
-    //         let item = Number(tokens.shift());
-    //         let idx = nums.indexOf(item);
-    //         console.log(idx);
-    //     } else if (action === "remove") {
-    //         let idx = Number(tokens.shift());
-    //         nums.splice(idx, 1);
-    //     } else if (action === "shift") {
-    //         let rotations = Number(tokens.shift())
-    //         for (let curRot = 1; curRot <= rotations; curRot++) {
-    //             let firstItem = nums.shift()
-    //             nums.push(firstItem)
-    //         }
-    //     } else if (action === "sumPairs") {
-    //         let pairedNums = [];
-    //         for (let i = 0; i < nums.length; i += 2) {
-    //             if (i + 1 < nums.length) {
-    //                 let pairSum = nums[i] + nums[i + 1];
-    //                 pairedNums.push(pairSum);
-    //             } else {
-    //                 pairedNums.push(nums[i]);
-    //             }
-    //         }
-    //         nums = pairedNums;
-    //     } else if (action === "print") {
-    //         console.log(`[ ${nums.join(`, `)} ]`);
-    //     }
-    // }
+    
 }
  //arrayManipulator([1, 2, 4, 5, 6, 7],
    //  ['add 1 8', 'contains 1', 'contains 3', 'print']);
